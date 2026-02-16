50c1592a8b71c3476423134ac05ab3923616ea71
JorDnn0 Mac <90001659+JorDnn0@users.noreply.github.com>
2026-02-16 14:09:49 +0000

    render cube svg

--- Class1.cs
+++ Class1.cs
@@ -1,14 +1,14 @@
 ﻿namespace cad_git;
 
 using VectSharp;
-
+using VectSharp.ThreeD;
 using VectSharp.SVG;
 public class Class1
 {
     // Entry point for the application
     static void Main(string[] args)
     {
-        testVectSharp();
+        test3D();
     }
 
     static void testVectSharp () {
@@ -44,5 +44,36 @@ public class Class1
 
         // Save the page as an SVG document.
         page.SaveAsSVG(outputFile); 
+    }
+    
+
+    static void test3D()
+    {
+        // Create a scene to contain 3D objects
+        Scene scene = new Scene();
+
+        // The static ObjectFactory class can be used to create "complex" 3D objects, such as a cube.
+        // These are returned as a list of triangles that can be added to the scene.
+        scene.AddRange(ObjectFactory.CreateCube(new Point3D(0, 0, 0), 100,
+                                                new IMaterial[] {
+                                                    new PhongMaterial(Colour.FromRgb(0, 158, 115))
+                                                }));
+
+        // The light will be used to illuminate the object(s) in the scene and make them actually visible.
+        ParallelLightSource light = new ParallelLightSource(0.5, new NormalizedVector3D(1, 2, 3));
+
+        // A camera that renders the scene using a perspective projection.
+        PerspectiveCamera camera = new PerspectiveCamera(new Point3D(-200, -200, -300),
+                                                        new NormalizedVector3D(2, 2, 3), 50,
+                                                        new Size(30, 30), 1);
+
+        // A renderer to render the scene as a vector image.
+        VectorRenderer renderer = new VectorRenderer() { DefaultOverFill = 0.02, ResamplingMaxSize = 1 };
+
+        // Render the scene.
+        Page pag = renderer.Render(scene, new ILightSource[] { light }, camera);
+
+        // Save as an SVG image.
+        pag.SaveAsSVG("Cube.svg");
     }
 }
